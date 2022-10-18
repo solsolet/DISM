f819eab1f65d9d9d844f69f5ee9acef559781cea
solsolet <gsl21@alu.ua.es>
2022-10-18 12:03:53 +0200

    Primer enviament

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Inicio', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Listado', url: '/folder/Outbox', icon: 'list' },
+    { title: 'Gestión', url: '/folder/Favorites', icon: 'create' },
+    { title: 'Contacto', url: '/folder/Archived', icon: 'call' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  /* public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];*/constructor() {}
 }
